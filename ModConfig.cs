1f93eb9702fa243237bad87e1d76148942f94732
xynerorias <axelmalmgren1@gmail.com>
2025-11-15 00:53:06 +0100

    first steps

--- ModConfig.cs
+++ ModConfig.cs
@@ -2,5 +2,26 @@ namespace pierres_roulette;
 
 public class ModConfig
 {
-    
+    // Master switch
+    public bool ModEnabled { get; set; } = true;
+
+    // Wether or not the mod affects the JojaMart
+    //Default: false
+    public bool JojaEnabled { get; set; } = false;
+
+    // // The shop owners to be affected by the mod. Their name must be their ShopMenu.portaitPerson.Name
+    // // Default: ["Pierre"]
+    // public string[] Owners { get; set; } = { "Pierre" };
+    //
+    // // Cycle between "OnlySeeds" to affect only the seeds, "OnlySaplings" to affect only the saplings or "Both" to affect them both
+    // // Default: "Both" 
+    // public string Mode { get; set; } = "Both";
+    //
+    // // How much seeds does the shops have in stock every day. Set to 0 to disable. Ignored if mode is "OnlySaplings"
+    // // Default: 5
+    // public int SeedStock { get; set; } = 5;
+    //
+    // //How much saplings does the shop have in stock every day. Set to 0 to disable. Ignored if mode is "OnlyCrops"
+    // // Default: 2
+    // public int SaplingStock { get; set; } = 2;
 }
--- ModEntry.cs
+++ ModEntry.cs
@@ -1,6 +1,40 @@
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+
 namespace pierres_roulette;
 
-public class ModEntry
+internal sealed class ModEntry : Mod
 {
-    
+    private ModConfig? _config;
+
+    public override void Entry(IModHelper helper)
+    {
+        _config = helper.ReadConfig<ModConfig>();
+        helper.Events.Input.ButtonPressed += OnButtonPressed;
+        helper.Events.Display.MenuChanged += MenuChanged;
+    }
+
+    private void OnButtonPressed(object? sender, ButtonPressedEventArgs eventArgs)
+    {
+        if (!Context.IsWorldReady)
+            return;
+        Monitor.Log($"{Game1.player.Name} pressed {eventArgs.Button}.", LogLevel.Debug);
+    }
+
+
+    private void MenuChanged(object? sender, MenuChangedEventArgs eventArgs)
+    {
+        if (_config != null && !_config.ModEnabled)
+            return;
+
+        if (eventArgs.NewMenu is ShopMenu shopMenu)
+            if (!shopMenu.categoriesToSellHere.Contains(-74)) // -74 is the category for seeds
+            {
+                var categories = shopMenu.categoriesToSellHere.ToArray();
+                Monitor.Log("Shop categories: " + string.Join(", ", categories), LogLevel.Debug);
+                Monitor.Log("Shop does not sell seeds, skipping...", LogLevel.Debug);
+            }
+    }
 }
