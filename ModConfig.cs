aba346f7493444859c76f930f995715ee6a151a7
xynerorias <axelmalmgren1@gmail.com>
2025-11-15 17:48:56 +0100

    Mod is working again

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,27 +1,28 @@
 namespace pierres_roulette;
 
-public class ModConfig
+public sealed class ModConfig
 {
     // Master switch
+    // Default: true
     public bool ModEnabled { get; set; } = true;
 
-    // Wether or not the mod affects the JojaMart
-    //Default: false
-    public bool JojaEnabled { get; set; } = false;
+    // The shop owners to be affected by the mod.
+    // Default: ["Pierre"]
+    public string[] Owners { get; set; } = { "Pierre" };
 
-    // // The shop owners to be affected by the mod. Their name must be their ShopMenu.portaitPerson.Name
-    // // Default: ["Pierre"]
-    // public string[] Owners { get; set; } = { "Pierre" };
-    //
-    // // Cycle between "OnlySeeds" to affect only the seeds, "OnlySaplings" to affect only the saplings or "Both" to affect them both
-    // // Default: "Both" 
-    // public string Mode { get; set; } = "Both";
-    //
-    // // How much seeds does the shops have in stock every day. Set to 0 to disable. Ignored if mode is "OnlySaplings"
-    // // Default: 5
-    // public int SeedStock { get; set; } = 5;
-    //
-    // //How much saplings does the shop have in stock every day. Set to 0 to disable. Ignored if mode is "OnlyCrops"
-    // // Default: 2
-    // public int SaplingStock { get; set; } = 2;
+    // Enable/disable seeds being affected
+    //Default: true
+    public bool SeedsEnabled { get; set; } = true;
+
+    // Enable/disable saplings being affected
+    // Default: true
+    public bool SaplingsEnabled { get; set; } = true;
+
+    // How much seeds does the shops have in stock every day. Set to -1 to disable. Does nothing if SeedsEnabled is false.
+    // Default: 5
+    public int SeedStock { get; set; } = 5;
+
+    //How much saplings does the shop have in stock every day. Set to -1 to disable. Does nothing if SaplingsEnabled is false.
+    // Default: 2
+    public int SaplingStock { get; set; } = 2;
 }
--- ModEntry.cs
+++ ModEntry.cs
@@ -12,29 +12,107 @@ internal sealed class ModEntry : Mod
     public override void Entry(IModHelper helper)
     {
         _config = helper.ReadConfig<ModConfig>();
-        helper.Events.Input.ButtonPressed += OnButtonPressed;
         helper.Events.Display.MenuChanged += MenuChanged;
     }
 
-    private void OnButtonPressed(object? sender, ButtonPressedEventArgs eventArgs)
-    {
-        if (!Context.IsWorldReady)
-            return;
-        Monitor.Log($"{Game1.player.Name} pressed {eventArgs.Button}.", LogLevel.Debug);
-    }
-
-
     private void MenuChanged(object? sender, MenuChangedEventArgs eventArgs)
     {
-        if (_config != null && !_config.ModEnabled)
+        if (eventArgs.NewMenu is not ShopMenu shopMenu)
             return;
 
-        if (eventArgs.NewMenu is ShopMenu shopMenu)
-            if (!shopMenu.categoriesToSellHere.Contains(-74)) // -74 is the category for seeds
+        if (_config is not { ModEnabled: true })
+            return;
+
+        if (shopMenu.forSale.OfType<Item>().All(i => i.Category != -74)) // -74 is the category for seeds
+        {
+            var categories = shopMenu.categoriesToSellHere.ToArray();
+            Monitor.Log("[Pierre's Roulette] Shop categories: " + string.Join(", ", categories), LogLevel.Debug);
+            Monitor.Log("[Pierre's Roulette] Shop does not sell seeds, skipping...", LogLevel.Debug);
+            return;
+        }
+
+        var seeds = shopMenu.forSale.FindAll(seed =>
+            seed is Item { Category: -74 } item && !item.Name.Contains("Sapling"));
+        var saplings = shopMenu.forSale.FindAll(sapling =>
+            sapling is Item { Category: -74 } item && item.Name.Contains("Sapling"));
+        var owners = shopMenu.ShopData.Owners;
+        owners.Remove(owners.FirstOrDefault()); // Remove default owner (always "None")
+        var rnd = new Random(Game1.Date.TotalDays + Game1.GetSaveGameName().GetHashCode());
+
+        Monitor.Log("[Pierre's Roulette] Shop seeds: " + string.Join(", ", seeds.Select(s => (s as Item)?.Name)),
+            LogLevel.Debug);
+        Monitor.Log("[Pierre's Roulette] Shop saplings: " + string.Join(", ", saplings.Select(s => (s as Item)?.Name)),
+            LogLevel.Debug);
+        Monitor.Log("[Pierre's Roulette] Shop owners: " + string.Join(", ", owners.Select(o => o.Name)),
+            LogLevel.Debug);
+
+        if (!owners.Any(o => _config.Owners.Contains(o.Name)))
+            return;
+
+        if (_config.SeedsEnabled)
+        {
+            RemoveRandomItems(rnd, _config.SeedStock, seeds, "Seeds");
+            Monitor.Log(
+                "[Pierre's Roulette] Remaining seeds: " + string.Join(", ", seeds.Select(s => (s as Item)?.Name)),
+                LogLevel.Debug);
+        }
+
+        if (_config.SaplingsEnabled)
+        {
+            RemoveRandomItems(rnd, _config.SaplingStock, saplings, "Saplings");
+            Monitor.Log(
+                "[Pierre's Roulette] Remaining saplings: " +
+                string.Join(", ", saplings.Select(s => (s as Item)?.Name)),
+                LogLevel.Debug);
+        }
+
+        Monitor.Log("[Pierre's Roulette] Old stock information: " + string.Join(", ",
+            shopMenu.itemPriceAndStock.Keys.Select(i => (i as Item)?.Name)), LogLevel.Debug);
+
+        var stockDict = new Dictionary<ISalable, ItemStockInformation>(shopMenu.itemPriceAndStock);
+        var stockList = new List<ISalable>(shopMenu.forSale);
+        var toRemove = new List<ISalable>(stockList.Where(i =>
+            i is Item { Category: -74 } && !seeds.Contains(i) && !saplings.Contains(i)));
+
+        Monitor.Log("[Pierre's Roulette] Items to remove: " + string.Join(", ",
+            toRemove.Select(i => (i as Item)?.Name)), LogLevel.Debug);
+        Monitor.Log("[Pierre's Roulette] Removing items...", LogLevel.Debug);
+
+        foreach (var item in toRemove)
+        {
+            stockDict.Remove(item);
+            stockList.Remove(item);
+        }
+
+        shopMenu.itemPriceAndStock = stockDict;
+        shopMenu.forSale = stockList;
+
+        Monitor.Log("[Pierre's Roulette] New stock information: " + string.Join(", ",
+            shopMenu.itemPriceAndStock.Keys.Select(i => (i as Item)?.Name)), LogLevel.Debug);
+    }
+
+    private void RemoveRandomItems(Random rnd, int amount, List<ISalable> items, string category)
+    {
+        if (amount < 0)
+        {
+            Monitor.Log(
+                "[Pierre's Roulette] Not removing items of category " + category, LogLevel.Debug);
+            return;
+        }
+
+        if (amount > items.Count)
+        {
+            Monitor.Log("[Pierre's Roulette] More items to remove than stock, removing entire stock", LogLevel.Debug);
+            items.Clear();
+        }
+        else
+        {
+            while (items.Count > amount)
             {
-                var categories = shopMenu.categoriesToSellHere.ToArray();
-                Monitor.Log("Shop categories: " + string.Join(", ", categories), LogLevel.Debug);
-                Monitor.Log("Shop does not sell seeds, skipping...", LogLevel.Debug);
+                var indexToRemove = rnd.Next(items.Count);
+                Monitor.Log($"Removed item: {items[indexToRemove].Name}", LogLevel.Debug);
+                items.RemoveAt(indexToRemove);
             }
+        }
     }
 }
